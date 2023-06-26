583880e5f4f99f19f3e578bf3c8f9c4272c1f453
Samy Nitsche <samy.nitsche@telekom.de>
2023-06-26 17:27:52 +0200

    feat(controller): refactored pod controller

--- src/controller/pod.py
+++ src/controller/pod.py
@@ -1,66 +1,120 @@
-from __future__ import annotations
 from typing import Any
-from contextlib import suppress
+import logging
 
-from controller.pod_config import PodConfig, PodConfigType
-from controller.routing import CurrentRouting, DesiredRouting
+from client import KubernetesClient
+from controller.schema.route import Route, RouteMissingError, RouteExistingError
+from controller.netroute import NetRoute
 
+logger = logging.getLogger(__name__)
 
 class Pod:
-    """Class representing a K8s Pod which holds current and desired config"""
 
-    PODS: dict[tuple[str, str], Pod] = {}
+    client = KubernetesClient()
 
-    def __init__(self, namespace: str, name: str):
-        self.namespace = namespace
-        self.name = name
+    def __init__(self, manifest: dict[str, Any]) -> None:
+        self.metadata = manifest.metadata
+        self.name = self.metadata.name
+        self.namespace = self.metadata.namespace
+    
+        self.spec = manifest.spec
 
-        self.current_routes = CurrentRouting(
-            v4_routes=[], v6_routes=[], pod_name=self.name, pod_namespace=self.namespace
-        )
-        self.desired_routes = DesiredRouting(v4_routes=[], v6_routes=[])
-
-        self.current_config: dict[PodConfigType, PodConfig] = {
-            "IPV4_ROUTES": self.current_routes.v4_routes,
-            "IPV6_ROUTES": self.current_routes.v6_routes,
-        }
-
-        self.desired_config: dict[PodConfigType, PodConfig] = {
-            "IPV4_ROUTES": self.desired_routes.v4_routes,
-            "IPV6_ROUTES": self.desired_routes.v6_routes,
-        }
-        self.PODS[((namespace, name))] = self
-
-    def __repr__(self):
-        return f"Pod({self.namespace}, {self.name})"
-
+    def __str__(self):
+        return f"Pod({self.namespace}/{self.name})"
+    
     @classmethod
-    def from_name(cls, namespace: str, name: str) -> Pod:
-        return cls.PODS.get((namespace, name)) or cls(namespace, name)
+    def get_pod(cls, namespace: str, name: str) -> 'Pod':
+        pod = cls.client.core_api.read_namespaced_pod(name, namespace)
+        return cls(pod)
+    
+    @classmethod
+    def list_pods(cls, namespace: str) -> list['Pod']:
+        pods = cls.client.core_api.list_namespaced_pod(namespace).items
+        return [Pod(pod) for pod in pods]
+    
+    @classmethod
+    def list_all_observed_pods(cls) -> list['Pod']:
+        pods = cls.list_pods(namespace='')
+        return [pod for pod in pods if pod.is_observed_by_netroute]
+    
+    @property
+    def is_observed_by_netroute(self) -> bool:
+        netroutes = NetRoute.list_netroutes(self.namespace)
+        for netroute in netroutes:
+            if netroute.spec.get('targetPod') == self.name and netroute.spec.get('targetNamespace') == self.namespace:
+                return True
+        return False
+    
+    async def annotate_pod_with_netroute(self, netroute: NetRoute) -> None:
+        netroute_annotation = self.metadata.annotations.get('netroute', '')
+        netroute_list = netroute_annotation.split(',') if netroute_annotation else []
+        if f'{netroute.name}@{netroute.namespace}' not in netroute_list:
+            netroute_list.append(f'{netroute.name}@{netroute.namespace}')
+        netroute_annotation = ','.join(netroute_list)
+        body = {
+            'metadata': {
+                'annotations': {
+                    'netroute': netroute_annotation,
+                },
+            },
+        }
+        self.client.core_api.patch_namespaced_pod(name=self.name, namespace=self.namespace, body=body)
+        logger.info(f"Annotated Pod {self.name} in namespace {self.namespace} with NetRoute {netroute.name} in namespace {netroute.namespace}.")
 
+    async def remove_netroute_annotation_from_pod(self, netroute: NetRoute) -> None:
+        netroute_annotation = self.metadata.annotations.get('netroute', '')
+        netroute_list = netroute_annotation.split(',') if netroute_annotation else []
+        if f'{netroute.name}@{netroute.namespace}' in netroute_list:
+            netroute_list.remove(f'{netroute.name}@{netroute.namespace}')
+        netroute_annotation = ','.join(netroute_list)
+        body = {
+            'metadata': {
+                'annotations': {
+                    'netroute': netroute_annotation,
+                },
+            },
+        }
+        self.client.core_api.patch_namespaced_pod(name=self.name, namespace=self.namespace, body=body)
+        logger.info(f"Removed NetRoute {netroute.name} in namespace {netroute.namespace} annotation from Pod {self.name} in namespace {self.namespace}")
+      
+    @classmethod
+    async def handle_netroute_on_startup(cls, netroute: NetRoute) -> None:
+        target_pod_name = netroute.spec.get('targetPod')
+        target_pod_namespace = netroute.spec.get('targetNamespace')
 
-    def refresh_current_routes(fn):
-        def wrapper(self, *args, **kwargs):
-            self.current_routes.get_config()
-            return fn(self, *args, **kwargs)
-        return wrapper
+        pod = cls.get_pod(target_pod_namespace, target_pod_name)
+        await pod.annotate_pod_with_netroute(netroute)      
 
+    @staticmethod
+    def deserialize_routing_table(routing_table: str, ip_version: int):
+        """Deserializes the string representation of the routing table"""
+        fn = Route.deserialize_v4_route if ip_version == 4 else Route.deserialize_v6_route
+        return [
+            fn(route)
+            for route in reversed(routing_table.splitlines())
+            if not (route.startswith("Destination")) and not (route.startswith("Kernel"))
+        ]
+    
+    def get_routes(self):
+        """Gets routes from a Pod via `route -n` and `route -A inet6 -n`"""
+        self.routes = []
+        self.routes.extend(self.deserialize_routing_table(self.client.exec_command(self.name, self.namespace, "route -n"), 4))
+        self.routes.extend(self.deserialize_routing_table(self.client.exec_command(self.name, self.namespace, "route -A inet6 -n"), 6))
 
-    @refresh_current_routes
-    def add_desired_routes(self, config: PodConfig) -> None:
-        self.desired_routes.add_config(config)
-        if config not in self.current_routes:
-            self.current_routes.add_config(config)
+    def add_route(self, route: Route) -> None:
+        """Adds a route to a Pod. If a route is already existing or the route was not successfully added an Exception is raised"""
+        self.get_routes()
+        if route not in self.routes:
+            self.client.exec_command(self.name, self.namespace, command=route.serialize_add_cmd())
+            self.get_routes()
+            if not route in self.routes:
+                raise RouteMissingError(route.network, route.gateway)
 
-    @refresh_current_routes
-    def remove_desired_routes(self, config: PodConfig, prune: bool) -> None:
-        with suppress(ValueError):
-            self.desired_routes.remove_config(config)
-        if prune:
-            self.current_routes.remove_config(config)
-
-    @refresh_current_routes
-    def reconcile_routes(self) -> None:
-        for config in self.desired_routes:
-            if config not in self.current_routes:
-                self.current_routes.add_config(config)
+    def remove_route(self, route: Route) -> None:
+        """Removes a route from a Pod. If a route is not existing or the route was not successfully removed an Exception is raised"""
+        self.get_routes()
+        if route not in self.routes:
+            raise RouteMissingError(route.network, route.gateway)
+        self.client.exec_command(self.name, self.namespace, command=route.serialize_delete_cmd())
+        self.get_routes()
+        if route in self.routes:
+            raise RouteExistingError(route.network, route.gateway)
