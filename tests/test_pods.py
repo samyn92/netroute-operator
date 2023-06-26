f2ed8393d573eeb4ebeeb4c637e1e3e36856d05e
Samy Nitsche <samy.nitsche@telekom.de>
2023-06-26 17:26:54 +0200

    chore(tests): removed

--- tests/test_pods.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from src.controller.pod import Pod
-
-
-class TestPods(unittest.TestCase):
-       
-    def test_pod(self):
-        assert type(Pod("default", "busybox-test")) is Pod
